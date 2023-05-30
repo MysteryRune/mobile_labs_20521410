cf956ead76be6896ab10fa574d01b7ffefceb3ba
MysteryRune <20521410@gm.uit.edu.vn>
2023-05-30 23:23:58 +0700

    update 3

--- app/src/main/java/uit/huyphambuinhat/lab04/RegisterActivity.java
+++ app/src/main/java/uit/huyphambuinhat/lab04/RegisterActivity.java
@@ -1,12 +1,17 @@
 package uit.huyphambuinhat.lab04;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.AsyncListUtil;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -20,6 +25,13 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -90,28 +102,30 @@ public class RegisterActivity extends AppCompatActivity {
                 ){
                     showPopupWindowRegisterSuccessfully(view);
 
-//                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-//                    // Create a new user with a first and last name
-//                    Map<String, Object> user = new HashMap<>();
-//                    user.put("first", "Ada");
-//                    user.put("last", "Lovelace");
-//                    user.put("born", 1815);
-//
-//                    // Add a new document with a generated ID
-//                    db.collection("users")
-//                            .add(user)
-//                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                                @Override
-//                                public void onSuccess(DocumentReference documentReference) {
-//                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-//                                }
-//                            })
-//                            .addOnFailureListener(new OnFailureListener() {
-//                                @Override
-//                                public void onFailure(@NonNull Exception e) {
-//                                    Log.w(TAG, "Error adding document", e);
-//                                }
-//                            });
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    // Create a new user with a first and last name
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("fullname", fullName);
+                    user.put("phoneNumber", phoneNumber);
+                    user.put("username", username);
+                    user.put("password", password);
+
+
+                    // Add a new document with a generated ID
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
 
 
                     Intent intent = new Intent(RegisterActivity.this,
@@ -192,4 +206,6 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
