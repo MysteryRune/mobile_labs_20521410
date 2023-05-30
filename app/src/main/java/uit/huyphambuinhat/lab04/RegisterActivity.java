15e572699122c397ed224f0fc6cdfc788203a246
MysteryRune <20521410@gm.uit.edu.vn>
2023-05-30 22:54:43 +0700

    update 2

--- app/src/main/java/uit/huyphambuinhat/lab04/RegisterActivity.java
+++ app/src/main/java/uit/huyphambuinhat/lab04/RegisterActivity.java
@@ -20,6 +20,11 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
     public void HideSystemStatusBar(int AndroidVersion) {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -85,7 +90,29 @@ public class RegisterActivity extends AppCompatActivity {
                 ){
                     showPopupWindowRegisterSuccessfully(view);
 
-                    
+//                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                    // Create a new user with a first and last name
+//                    Map<String, Object> user = new HashMap<>();
+//                    user.put("first", "Ada");
+//                    user.put("last", "Lovelace");
+//                    user.put("born", 1815);
+//
+//                    // Add a new document with a generated ID
+//                    db.collection("users")
+//                            .add(user)
+//                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                                @Override
+//                                public void onSuccess(DocumentReference documentReference) {
+//                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                                }
+//                            })
+//                            .addOnFailureListener(new OnFailureListener() {
+//                                @Override
+//                                public void onFailure(@NonNull Exception e) {
+//                                    Log.w(TAG, "Error adding document", e);
+//                                }
+//                            });
+
 
                     Intent intent = new Intent(RegisterActivity.this,
                             LoginActivity.class);
