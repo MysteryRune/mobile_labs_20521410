1742c4282a59b54054d011b3d85d8ac62cdcca01
MysteryRune <20521410@gm.uit.edu.vn>
2023-06-13 22:22:42 +0700

    Update 2

--- app/src/main/java/uit/huyphambuinhat/lab05/HomePageActivity.java
+++ app/src/main/java/uit/huyphambuinhat/lab05/HomePageActivity.java
@@ -2,14 +2,131 @@ package uit.huyphambuinhat.lab05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HomePageActivity extends AppCompatActivity{
+
+    private Button btnFadeInXml, btnFadeInCode, btnFadeOutXml, btnFadeOutCode, btnBlinkXml,
+            btnBlinkCode, btnZoomInXml, btnZoomInCode, btnZoomOutXml, btnZoomOutCode, btnRotateXml,
+            btnRotateCode, btnMoveXml, btnMoveCode, btnSlideUpXml, btnSlideUpCode, btnBounceXml,
+            btnBounceCode, btnCombineXml, btnCombineCode;
+    private ImageView ivUitLogo;
+    private Animation.AnimationListener animationListener;
+
+    private void findViewsByIds() {
+        ivUitLogo = (ImageView) findViewById(R.id.iv_uit_logo);
+        btnFadeInXml = (Button) findViewById(R.id.btn_fade_in_xml);
+        btnFadeInCode = (Button) findViewById(R.id.btn_fade_in_code);
+        btnFadeOutXml = (Button) findViewById(R.id.btn_fade_out_xml);
+        btnFadeOutCode = (Button) findViewById(R.id.btn_fade_out_code);
+        btnBlinkXml = (Button) findViewById(R.id.btn_blink_xml);
+        btnBlinkCode = (Button) findViewById(R.id.btn_blink_code);
+        btnZoomInXml = (Button) findViewById(R.id.btn_zoom_in_xml);
+        btnZoomInCode = (Button) findViewById(R.id.btn_zoom_in_code);
+        btnZoomOutXml = (Button) findViewById(R.id.btn_zoom_out_xml);
+        btnZoomOutCode = (Button) findViewById(R.id.btn_zoom_out_code);
+        btnRotateXml = (Button) findViewById(R.id.btn_rotate_xml);
+        btnRotateCode = (Button) findViewById(R.id.btn_rotate_code);
+        btnMoveXml = (Button) findViewById(R.id.btn_move_xml);
+        btnMoveCode = (Button) findViewById(R.id.btn_move_code);
+        btnSlideUpXml = (Button) findViewById(R.id.btn_slide_up_xml);
+        btnSlideUpCode = (Button) findViewById(R.id.btn_slide_up_code);
+        btnBounceXml = (Button) findViewById(R.id.btn_bounce_xml);
+        btnBounceCode = (Button) findViewById(R.id.btn_bounce_code);
+        btnCombineXml = (Button) findViewById(R.id.btn_combine_xml);
+        btnCombineCode = (Button) findViewById(R.id.btn_combine_code);
+    }
+
+    private void initVariables() {
+        animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Toast.makeText(getApplicationContext(), "Animation Stopped",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+    }
+
+    private void handleClickAnimationXml(Button btn, int animId) {
+        // Fade In animation
+        final Animation animation = AnimationUtils.loadAnimation(HomePageActivity.this,
+                animId);
+        animation.setAnimationListener(animationListener);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ivUitLogo.startAnimation(animation);
+            }
+        });
+    }
+
+    public void HideSystemStatusBar(int AndroidVersion) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        if (AndroidVersion < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+    public void HideSystemNavigationBar() {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int AndroidVersion = Build.VERSION.SDK_INT;
+//        HideSystemStatusBar(AndroidVersion);
+        HideSystemNavigationBar();
+
         setContentView(R.layout.activity_home_page);
+
+        findViewsByIds();
+        initVariables();
+
+        handleClickAnimationXml(btnFadeInXml, R.xml.anim_fade_in);
+        handleClickAnimationXml(btnFadeOutXml, R.xml.anim_fade_out);
+        handleClickAnimationXml(btnBlinkXml, R.xml.anim_blink);
+        handleClickAnimationXml(btnZoomInXml, R.xml.anim_zoom_in);
+        handleClickAnimationXml(btnZoomOutXml, R.xml.anim_zoom_out);
+        handleClickAnimationXml(btnRotateXml, R.xml.anim_rotate);
+        handleClickAnimationXml(btnMoveXml, R.xml.anim_move);
+        handleClickAnimationXml(btnSlideUpXml, R.xml.anim_slide_up);
+        handleClickAnimationXml(btnBounceXml, R.xml.anim_bounce);
+        handleClickAnimationXml(btnCombineCode, R.xml.anim_combine);
     }
 
 }
